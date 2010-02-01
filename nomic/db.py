aa916d6d11a60e9d7578c499d1c12ce9024059f8
Noah Kantrowitz <noah@coderanger.net>
2010-01-31 23:59:19 -0800

    Add the userbar back in.

--- nomic/db.py
+++ nomic/db.py
@@ -5,9 +5,3 @@ File = __NomicFile__
 class User(db.Model):
     user = db.UserProperty()
     score = db.IntegerProperty(default=0)
-    
-    @property
-    def nickname(self):
-        if self.user:
-            return self.user.nickname()
-        return ''

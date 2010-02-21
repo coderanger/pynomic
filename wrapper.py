37c9aec7d3cc1ae447dec072580086921e387a30
Noah Kantrowitz <noah@coderanger.net>
2010-02-21 17:43:57 -0500

    Add support for actual packages and subpackages in the DB filesystem.

--- wrapper.py
+++ wrapper.py
@@ -24,15 +24,17 @@ class Loader(object):
     def load_module(self, name):
         logging.debug('Trying to load %s', name)
         path = name[6:].replace('.', '/') + '.py'
-        ispkg = '.' not in name
+        ispkg = self.is_package(name)
         do_reload = True
-        if not ispkg:
+        if ispkg:
+            code_file = ispkg
+        else:
             code_file = File.from_path(path)
             if not code_file:
                 raise ImportError, 'datastore:%s not found (%s)'%(path, name)
-            mod = sys.modules.get(name)
-            if mod is not None and mod.__mtime__ >= code_file.last_modified:
-                do_reload = False
+        mod = sys.modules.get(name)
+        if mod is not None and mod.__mtime__ >= code_file.last_modified:
+            do_reload = False
         
         if do_reload:
             logging.debug('Loading module %s', name)
@@ -41,10 +43,8 @@ class Loader(object):
             mod.__loader__ = self
             mod.__mtime__ = datetime.datetime.now()
             if ispkg:
-                mod.__path__ = []
-                code = ''
-            else:
-                code = 'from __future__ import absolute_import\n' + code_file.data + '\n\n'
+                mod.__path__ = [ispkg.path[:-12]]
+            code = 'from __future__ import absolute_import\n' + code_file.data + '\n\n'
             mod.__dict__['__NomicFile__'] = File
             compiled = compile(code, mod.__file__, 'exec')
             exec compiled in mod.__dict__
@@ -52,6 +52,32 @@ class Loader(object):
                 parent_name, child_name = name.rsplit('.', 1)
                 setattr(sys.modules[parent_name], child_name, mod)
         return sys.modules[name]
+    
+    def _path_to_modname(self, path):
+        pass
+    
+    def _modname_to_path(self, name):
+        """Convert 'nomic.main' to 'main.py'."""
+        return name[6:].replace('.', '/') + '.py'
+    
+    def _pkgname_to_path(self, name):
+        """Convert 'nomic.main' to 'main/__init__.py'."""
+        return (name[6:].replace('.', '/') + '/__init__.py').lstrip('/')
+    
+    def get_source(self, name):
+        pkg = self.is_package(name)
+        if pkg:
+            return pkg.data
+        path = self._modname_to_path(name)
+        code_file = File.from_path(path)
+        if not code_file:
+            return None
+        return file.data
+    
+    def is_package(self, name):
+        path = self._pkgname_to_path(name)
+        return File.from_path(path)
+        
 
 class MetaImporter(object):
     def __init__(self):
@@ -64,7 +90,7 @@ class MetaImporter(object):
     
     @classmethod
     def install(cls):
-        sys.meta_path.append(cls())
+        sys.meta_path.insert(0, cls())
 
 
     
